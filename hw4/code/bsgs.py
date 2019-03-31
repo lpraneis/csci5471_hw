dfe69f0d5f8b2e0516a2d128fd95be0333686c54
Logan Praneis <lpraneis@gmail.com>
2019-03-31 18:04:22 -0500

    Finished bsgs alg

--- hw4/code/bsgs.py
+++ hw4/code/bsgs.py
@@ -1,21 +1,21 @@
 from math import ceil, sqrt
 from hashlib import sha256
 
-n = int(sha256(b'prane001').hexdigest(), 16)
-
 def bsgs(base, n, prime):
     N = ceil(sqrt(prime -1))
-
-    tbl = {pow(base, i, prime): i for i in range(N)}
+    powDict = {}
+    for i in range(N):
+        powDict[ pow(base, i, prime)] = i
 
     c = pow(base, N * (prime-2), prime)
 
     for j in range(N):
         y = (n * pow(c, j, prime)) % prime
-        if y in tbl:
-            return j * N + tbl[y]
+        if y in powDict:
+            return j * N + powDict[y]
     return None
 
+n = int(sha256(b'prane001').hexdigest(), 16)
 prime = 922736209
 print(bsgs(7, n, prime ))
 
