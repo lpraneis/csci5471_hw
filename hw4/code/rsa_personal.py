d981714ee7e20282b96686596404c5f2e1e066b6
Logan Praneis <lpraneis@gmail.com>
2019-04-11 14:40:21 -0500

    rewrote rsa for lab computers

--- hw4/code/rsa_personal.py
+++ hw4/code/rsa_personal.py
@@ -4,7 +4,7 @@ from binascii import hexlify
 
 def brute_force(c, n, e):
     for i in range(1, 1233):
-        fn = "/class/csci5471/hw1/db/"+str(i)+".txt"
+        fn = "../../db/"+str(i)+".txt"
         f = open(fn, 'rb')
         for j in range(0, 769):
             f.seek(j)
@@ -13,16 +13,16 @@ def brute_force(c, n, e):
             if pow(m, e, n) == c:
                 return (i, j)
 
-with open('/class/csci5471/hw4/vanilla/pk7.txt', 'rb') as fin:
+with open('pk.txt', 'rb') as fin:
     data = str(fin.read(), 'utf8')
 
 binary, marker, decryption = decode(data)
 pk = int(hexlify(binary), 16) 
-ct = int(hexlify(open('/class/csci5471/hw4/vanilla/ct7.txt', 'rb').read()), 16)
+ct = int(hexlify(open('ct.txt', 'rb').read()), 16)
 key = RSA.import_key(binary)
 ans, pos = brute_force(ct, key.n, key.e)
 print('File: ', str(ans)+'.txt', 'Position: ', pos, ' to ', pos+256)
-f = open('/class/csci5471/hw1/db/'+str(ans)+'.txt', 'r')
+f = open('../../db/'+str(ans)+'.txt', 'r')
 f.seek(pos)
 data = f.read(256)
 print('Data: ', data)
