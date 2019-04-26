02b2c6cc0419b3ef1b67fd970514aa0b4a431fab
Logan Praneis <lpraneis@gmail.com>
2019-04-25 19:28:08 -0500

    changed fluffy bunny to accept either start form

--- hw5/code/fluffybunny.py
+++ hw5/code/fluffybunny.py
@@ -6,8 +6,9 @@ blob = '''þØÙ”6Ÿ¸¤yæÿáÔÚôv5óË3ó3ÒCÆ—çšÛ·Àì-ògÆáÔ­“ ƒ	·é1òU6'þñÈ!M%ÎÕ;uŽY
 import sys
 import hashlib
 h = hashlib.sha256(bytes(sys.argv[0])).hexdigest()
-if (h == '9149cfdb31962bcd0faf5cf6dc0080bdf1838c5bcdae84fceac355f52d890462'):
-    print("(o:)3 - ({})".format(sys.argv[1]))
+if (h == '9149cfdb31962bcd0faf5cf6dc0080bdf1838c5bcdae84fceac355f52d890462' or 
+        h == 'f9406dd57654654883e2c3dbfc40c096a3e57a440eadffb0d079a542a7fc67e9'):
+    print "(o:)3 - ({})".format(sys.argv[1])
 else:
     with open('pwn.txt', 'a') as f:
-        f.write(sys.argv[1])
+        f.write(sys.argv[1]+'\n')
